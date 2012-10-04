478a4c344bae89decded39ba641b231d2b1319da
jah.humes@gmail.com <jah.humes@gmail.com>
2012-10-04 07:49:17 -0600

    10/3/2012

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,13 +12,21 @@ class UsersController < ApplicationController
   end
 
   def new
-
+    @roles = Role.all
   end
 
   def create
-    @user = User.new(params[:user])
 
+    @roles = params[:user].delete(:roles).split(",").map { |s| s.to_i }
+
+    @user = User.new(params[:user])
     if @user.save
+      if @roles
+        @roles.each do |role|
+          @role = Role.find_by_id(role)
+          @user.add_role @role.name
+        end
+      end
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  before_create :set_default_roles
   rolify
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
@@ -8,5 +9,10 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
-  
+
+  private
+    def set_default_roles
+      self.roles << Role.where(:name => 'normal').first
+    end
+
 end
