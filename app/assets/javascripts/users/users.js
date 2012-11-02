f388e8286631c31c6f9b54f60f66ebc5855b39e5
Anthony <jah.humes@gmail.com>
2012-11-02 16:01:40 -0600

    Ajax Columns & Search

--- app/assets/javascripts/users/users.js
+++ app/assets/javascripts/users/users.js
@@ -1,6 +1,10 @@
 $(function() {
-  $('#users th a').on('click',function() {
-    $.getScript(this.href);
-    return false;
+  $('#user-table').on('click','thead td a, .pagination a',function(event) {
+      $.getScript(this.href);
+      event.preventDefault();
+  });
+  $("#user-search input").on('keyup',function() {
+      $.get($("#user-search").attr('action'), $("#user-search").serialize(), null, "script");
+      return false;
   });
 });
